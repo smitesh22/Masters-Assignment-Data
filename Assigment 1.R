a82ef5bd936202ac97db68c6c1c3149159f35378
smitesh22 <smitesh22@gmail.com>
2022-09-16 15:16:14 +0100

    Update Assigment 1.R

--- Assigment 1.R
+++ Assigment 1.R
@@ -10,7 +10,20 @@ tail(temps)
 
 sum(temps > mean(temps))
 
-names[max(temps)]
+max_temp <- max(temps)
+names(temps)[temps == max(temps)]
 
 min(temps)
+names(temps)[temps == min(temps)]
+
+(w_warnings <- ifelse(temps <= 4, "Warning", "Normal"))
+
+temps[40:44]
+w_warnings[40:44]
+
+sum(ifelse(w_warnings == "Warning", TRUE, FALSE))
+
+names(w_warnings)[w_warnings == "Warning"]
+
+(tab <- table(w_warnings))
 

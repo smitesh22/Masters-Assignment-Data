53f1ae24078494ca63cc1cc38d7614d4b4afb3a4
smitesh22 <smitesh22@gmail.com>
2022-09-20 22:01:39 +0100

    Update Assigment 1.R

--- Programming for Data Analytics lab/Assigment 1.R
+++ Programming for Data Analytics lab/Assigment 1.R
@@ -8,30 +8,34 @@ head(temps)
 
 tail(temps)
 
-# Gt mean
 
-sum(temps > mean(temps))
+gt_mean<- sum(temps > mean(temps))
+cat("1. Number of days with temperature greater than mean :", gt_mean)
 
 max_temp <- max(temps)
 
 
-cat("Max temp was ", max(temps), " on ", names(temps)[temps == max(temps)]) 
+cat("2. Max temp was ", max(temps), " on day ", names(temps)[temps == max(temps)]) 
 
-cat("Min temp was ", min(temps), " on ", names(temps)[temps == min(temps)])
+cat("3. Min temp was ", min(temps), " on day ", names(temps)[temps == min(temps)])
 
 
 w_warnings <- ifelse(temps <= 4, "Warning", "Normal")
 
+print("4. Create parallel vecotor warning with values 'warning' and 'normal' : ")
 temps[40:44]
 w_warnings[40:44]
 
-cat("Number of days warning were issude : ", 
+cat("5. Number of days warning were issued : ", 
     sum(ifelse(w_warnings == "Warning", TRUE, FALSE)))
 
+print("6. Days when warning was issued")
 names(w_warnings)[w_warnings == "Warning"]
 
+print("7. Warnings in tabular format")
 (tab <- table(w_warnings))
 
+print("8.Use of rle function")
 rle_encoded <- rle(w_warnings)
 
 cat("Maximum days without warning : ",

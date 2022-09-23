ed2e108a3920ecdd7fee76e9727f4d1dc0c873a1
smitesh22 <smitesh22@gmail.com>
2022-09-23 23:16:50 +0100

    Update Assign 2 completed yay

--- Programming for Data Analytics lab/Assigment 2.R
+++ Programming for Data Analytics lab/Assigment 2.R
@@ -1,8 +1,7 @@
-
 conv_df_to_list <- function(dataframe){
   output = list()
   df_names <- names(dataframe)
-  for (i in range(1:nrow(dataframe))){
+  for (i in 1:nrow(dataframe)){
     output <- append(output, list(as.list(dataframe[i, ])))
   }
   names(output) <- paste0("R-", 1:length(output))
@@ -10,7 +9,7 @@ conv_df_to_list <- function(dataframe){
   return(output)
 }
 
-output <- conv_df_to_list(head(mtcars))
+output <- conv_df_to_list(head(Formaldehyde))
 
 str(output)
 

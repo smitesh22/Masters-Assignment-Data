7e69d75e11251991a6629489ee880933781c1636
smitesh22 <smitesh22@gmail.com>
2022-09-29 14:39:15 +0100

    Update Assigment 3.R

--- Programming for Data Analytics lab/Assigment 3.R
+++ Programming for Data Analytics lab/Assigment 3.R
@@ -1,3 +1,34 @@
 library(repurrrsive)
 
 str(sw_films)
+
+get_directors <- function(){
+  out <- lapply(sw_films, function(x){
+    x$director
+  })
+  
+  return(unlist(unique(out)))
+}
+
+dirs <- get_directors()
+dirs
+
+
+get_film_info <- function(x){
+  x <- "George Lucas"
+  out <- lapply(sw_films, function(y){
+                          if(y$director==x) list(y$director, 
+                                                      y$title,
+                                                      y$episode,
+                                                      y$opening,
+                                                      y$release_date)
+                          }
+                        )
+  
+  out <- target_list[lengths(out) != 0]
+  
+  return(out)
+}
+
+movies <- get_film_info("George Lucas")
+str(movies)

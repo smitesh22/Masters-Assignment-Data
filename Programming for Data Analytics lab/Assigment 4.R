bdf1d856b568558b1a27ebec434f84aea7af5484
smitesh22 <smitesh22@gmail.com>
2022-10-08 16:17:31 +0100

    zl

--- Programming for Data Analytics lab/Assigment 4.R
+++ Programming for Data Analytics lab/Assigment 4.R
@@ -5,6 +5,12 @@ CX103 <- rnorm(20,85,10)
 CX104 <- rnorm(20,45,10)
 CX105 <- rnorm(20,60,5)
 
+summarise(CX101)
+summarise(CX102)
+summarise(CX103)
+summarise(CX104)
+summarise(CX105)
+
 col_names <- paste0("CX10", 1:5)
 row_names <- paste0("Student_", 1:20)
 
@@ -21,21 +27,28 @@ mat_with_na = apply(mat, c(1,2), function(x){
    }
 })
 
-mean_val <- mean(mat_with_na[, "CX103"], na.rm = TRUE)
+#mean_val <- mean(mat_with_na[, "CX103"], na.rm = TRUE)
 
-mat_with_avg = apply(mat_with_na, c(1,2), function(x){
+
+mat_with_avg = apply(mat_with_na, c(1, 2), function(x){
+  col.means <- apply(mat_with_na, 2, mean, na.rm=T)
   if(is.na(x)){
-    x <- 
-  }
-  else{
+    x <- col.means[3]
+  }else{
     x <- x
   }
 })
 
+print(mat_with_avg)
+
 mean <- apply(mat, 1, mean)
-range <- apply(mat, 1, range)
+range <- apply(mat, 1, function(x){
+  x <- max(x) - min(x)
+})
 
 mat_with_mean <- cbind(mat_with_avg, mean)
 mat_with_range <- cbind(mat_with_mean, range)
 
-mat_with_avg
+
+max <- apply(mat_with_range, 2, max)['mean']
+mat_with_range[mat_with_range[, 'mean'] == max, ,drop= F]

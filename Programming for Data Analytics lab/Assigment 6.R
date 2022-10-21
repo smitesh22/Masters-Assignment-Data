90abc9513980f7b12eb13dc3a11d30af4cff589a
smitesh22 <smitesh22@gmail.com>
2022-10-21 01:10:09 +0100

    Update Assigment 6.R

--- Programming for Data Analytics lab/Assigment 6.R
+++ Programming for Data Analytics lab/Assigment 6.R
@@ -2,7 +2,23 @@ library(aimsir17)
 library(ggplot2)
 library(dplyr)
 
-observations %>% 
+s_data <- observations %>% 
   group_by(station, day, month) %>% 
-  summarise(TotalRain = sum(rain)) %>%
+  summarise(TotalRain = sum(rain), AvrTemp = mean(temp))
   
+glimpse(s_data)
+
+arranged_data<- s_data %>%
+  arrange(month)
+
+s_data_diff <- 
+  cbind(arranged_data,lag(arranged_data))
+
+s_data_diff <- s_data_diff[,c(1, 2, 3, 4, 5, 9, 10)] %>% 
+  mutate(RainDiff = TotalRain...4 - TotalRain...9,
+         MeanTempDiff = AvrTemp...5 - AvrTemp...10) 
+
+s_data_diff <- s_data_diff %>% select(station...1, day...2, month...3,
+                       TotalRain...4, AvrTemp...5, RainDiff, MeanTempDiff) %>%
+  mutate(AbsRainDiff = abs(RainDiff), AbsMeanTempDiff = abs(MeanTempDiff))
+

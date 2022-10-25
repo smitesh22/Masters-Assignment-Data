c0b373fc4a51099bec5610067c8aca748f98860c
smitesh22 <smitesh22@gmail.com>
2022-10-25 18:47:50 +0100

    Update ass 6 final commit

--- Programming for Data Analytics lab/Assigment 6.R
+++ Programming for Data Analytics lab/Assigment 6.R
@@ -1,30 +1,41 @@
+#name : Smitesh patil
+#std. id : 22223696
+
 library(aimsir17)
 library(ggplot2)
 library(dplyr)
 
 s_data <- observations %>% 
   group_by(station, day, month) %>% 
-  summarise(TotalRain = sum(rain), AvrTemp = mean(temp))
-  
+  summarise(TotalRain = sum(rain), AvrTemp = mean(temp)) %>%
+  ungroup()
+
+# s_data contains station-wise rain and temp data by month
+print(s_data)
+
+#glimpse of s_data
 glimpse(s_data)
 
-arranged_data<- s_data %>%
+s_data_diff <- s_data %>%
   arrange(month)
 
-s_data_diff <- 
-  cbind(arranged_data,lag(arranged_data))
+s_data_diff <- s_data_diff %>%
+  mutate(LagTotalRain = lag(s_data_diff['TotalRain']),
+         LagAvgTemp = lag(s_data_diff['AvrTemp'])) %>%
+  mutate(RainDiff = TotalRain - LagTotalRain,
+         MeanTempDiff = AvrTemp - LagAvgTemp) %>%
+  mutate(AbsRainDiff = abs(RainDiff),
+         AbsMeanTempDiff = abs(MeanTempDiff)) %>%
+  mutate(RainDiff = RainDiff[, 1],
+         AbsRainDiff = AbsRainDiff[, 1],
+         MeanTempDiff = MeanTempDiff[, 1],
+         AbsMeanTempDiff = AbsMeanTempDiff[, 1]) %>%
+  select(station, day, month, TotalRain, AvrTemp,
+         RainDiff, AbsRainDiff, MeanTempDiff, AbsMeanTempDiff)
 
-s_data_diff <- s_data_diff[,c(1, 2, 3, 4, 5, 9, 10)] %>% 
-  mutate(RainDiff = TotalRain...4 - TotalRain...9,
-         MeanTempDiff = AvrTemp...5 - AvrTemp...10) 
-
-s_data_diff <- s_data_diff %>% select(station...1, day...2, month...3,
-                       TotalRain...4, AvrTemp...5, RainDiff, MeanTempDiff) %>%
-  mutate(AbsRainDiff = abs(RainDiff), AbsMeanTempDiff = abs(MeanTempDiff)) %>%
-  select(station = station...1, day = day...2, month = month...3,
-         TotalRain = TotalRain...4, AvrTemp= AvrTemp...5, RainDiff, AbsRainDiff,
-         MeanTempDiff, AbsMeanTempDiff)
-
+#s_data_diff contains s_data with rain difference and temperature
+#compared to next day and absolute values
+print(s_data_diff)
 
 out <- s_data_diff %>%group_by(station, month) %>%
   summarise(AvgDiffTemp = mean(MeanTempDiff, na.rm=T),
@@ -36,6 +47,12 @@ out <- s_data_diff %>%group_by(station, month) %>%
             MinDiffRain = min(RainDiff, na.rm=T),
             MaxDiffRain = max(RainDiff, na.rm=T))
 
+#out contains mean, max, min and standard deviations of rain and tempature values 
+#grouped monthly
+
+print(out)
+
+# first plot for Temperature data
 ggplot(out,aes(x = month))+
   geom_line(aes(y = MinDiffTemp), colour="Blue") +
   geom_line(aes(y = MaxDiffTemp), colour="Blue") +
@@ -43,9 +60,12 @@ ggplot(out,aes(x = month))+
   geom_ribbon(aes(ymin = -SDDiffTemp, ymax = SDDiffTemp),fill="Red") +
   geom_point(aes(y = AvgDiffTemp))+
   geom_line(aes(y = AvgDiffTemp))+
-  coord_cartesian(ylim = c(5, -5))
+  coord_cartesian(ylim = c(5, -5))+
+  scale_x_continuous(breaks = seq(1, 12, by = 1))+
+  ylab("AvrDiffTemp")+
+  xlab("month")
 
-
+# second plot for Rain data
 ggplot(out,aes(x = month))+
   geom_line(aes(y = MinDiffRain), colour="Red") +
   geom_line(aes(y = MaxDiffRain), colour="Red") +
@@ -53,4 +73,7 @@ ggplot(out,aes(x = month))+
   geom_point(aes(y = AvgDiffRain))+
   geom_line(aes(y = AvgDiffRain))+
   facet_wrap(~station) +
-  coord_cartesian(ylim = c(20, -20))
+  coord_cartesian(ylim = c(20, -20)) +
+  scale_x_continuous(breaks = seq(1, 12, by = 1))+
+  ylab("AvgDiffRain")+
+  xlab("month")

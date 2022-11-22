bf12b57dea765ee1672128cd3a76c1351a61ce93
smitesh22 <smitesh22@gmail.com>
2022-11-22 11:47:25 +0000

    commit last week

--- Programming for Data Analytics lab/Assigment 9.R
+++ Programming for Data Analytics lab/Assigment 9.R
@@ -1,16 +1,45 @@
+library(purrr)
+library(dplyr)
 library(tidyverse)
 
 
-d1 <- mpg %>%
-  group_by(class) %>%
-  nest()
+d1 <- mpg %>% group_by(class)
 
-my_mpg_lms = function (x){
-  x %>% mutate(
-    lm = map(data, ~lm(displ ~ cty ,data = .))
-  )
+d1
+
+my_mpg_lms <- function(x){
+  main <- group_split(x) %>% map(~lm(cty~displ, data=.))
+  class(main) <- "my_mpg_lms"
+  names(main) <- unlist(group_keys(x))
+  main
 }
 
 mods1 <- my_mpg_lms(d1)
+mods1
+
+length(mods1)
+
+class(mods1)
 
 names(mods1)
+
+str(mods1[[1]])
+
+str(mods1[[7]])
+
+summary.my_mpg_lms <- function(x){
+  print("The following are the model groups")
+  print(names(x))
+  print("Here are the results")
+  for(mod in x){
+    print(summary(mod))
+  }
+  
+}
+summary(mods1)
+
+d2 <- mpg %>% group_by(manufacturer)
+mods2 <- my_mpg_lms(d2)
+names(mods2)
+
+summary(mods2)

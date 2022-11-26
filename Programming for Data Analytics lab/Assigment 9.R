d1c478a589d1bc8c963c38266b2f6578ff7e69fa
smitesh22 <smitesh22@gmail.com>
2022-11-26 09:09:34 +0000

    summary

--- Programming for Data Analytics lab/Assigment 9.R
+++ Programming for Data Analytics lab/Assigment 9.R
@@ -28,13 +28,18 @@ str(mods1[[1]])
 str(mods1[[7]])
 
 summary.my_mpg_lms <- function(x){
-  print("The following are the model groups")
-  print(names(x))
-  print("Here are the results")
-  for(mod in x){
-    print(summary(mod))
-  }
-  
+  cat("The following are the model groups \n")
+  cat(names(x))
+  cat("Here are the results \n")
+  i = 1
+  test <- walk2(names(x),x,~{
+    cat("Model #", i, 
+        "Group", .x, 
+        "Obs = ", paste(count(.y$model), "\n"))
+    print(summary(.y))
+    cat("============================================\n")
+    i <<- i+1
+  })
 }
 summary(mods1)
 
